4fd94c5d8a0d7e78b4511750f5908294c2914f36
Dmitry-Belonosov <belonosov.dmitry@gmail.com>
2023-03-14 13:33:08 +0300

    [MusicPlayer] add all files

---
+++ data.js
@@ -0,0 +1,42 @@
+let songs = [
+    {
+        name: 'Never let me down again',
+        path: 'musics/Song 1.mp3',
+        artist: 'Depeche mode',
+        cover: 'images/cover 1.png',
+        next: 1,
+        prev: ''
+    },
+    {
+        name: 'Personal Jesus',
+        path: 'musics/Song 2.mp3',
+        artist: 'Depeche mode',
+        cover: 'images/cover 2.png',
+        next: 2,
+        prev: 0
+    },
+    {
+        name: 'Strangelove',
+        path: 'musics/Song 3.mp3',
+        artist: 'Depeche mode',
+        cover: 'images/cover 3.png',
+        next: 3,
+        prev: 1
+    },
+    {
+        name: 'Rosernot',
+        path: 'musics/Song 4.mp3',
+        artist: 'Rammstein',
+        cover: 'images/cover 4.png',
+        next: 4,
+        prev: 2
+    },
+    {
+        name: 'Ohne dich',
+        path: 'musics/Song 5.mp3',
+        artist: 'Rammstein',
+        cover: 'images/cover 5.png',
+        next: '',
+        prev: 3
+    },
+]
